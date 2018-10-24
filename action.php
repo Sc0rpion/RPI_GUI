52728b19765362d65879e1c984ac26991510477d
Sc0rpion <7339871+Sc0rpion@users.noreply.github.com>
2018-10-24 12:49:31 +0600

    v0.02
    v0.02

--- action.php
+++ action.php
@@ -1,34 +1,42 @@
+<?php	if (isset($_GET['procedure']) && isset($_GET['ip'])) {
+
+		setcookie("package",isset($_GET['package'])?trim($_GET['package']):' ');
+		setcookie("ip",isset($_GET['ip'])?trim($_GET['ip']):' ');
+		setcookie("task_id",intval(isset($_GET['task_id'])?trim($_GET['task_id']):' '));}?>
+
+
 <!DOCTYPE HTML>
+
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>PS4 Remote Package Installer</title>
+  <title>PS4 Remote Package Installer GUI</title>
  </head>
  <body>
+	<center><h1>PS4 Remote Package Installer GUI</h1></center>
 	 <b>Установка PGK</b>
 <form action="action.php" method="get">
- <p>ip ps4: <input type="text" name="ip" value="<?php echo isset($_GET['ip'])?$_GET['ip']:' '; ?>"/></p>
- <p>url pkg (обязательно с http://): <input type="text" name="package" value="<?php echo isset($_GET['package'])?$_GET['package']:' '; ?>"/></p>
+ <p>ip ps4: <input type="text" name="ip" value="<?php echo isset($_COOKIE['ip'])?$_COOKIE['ip']:' '; ?>"/></p>
+ <p>url pkg (обязательно с http://): <input type="text" name="package" value="http://"/></p>
  <input type="hidden" name="procedure" value="install">
  <p><input type="submit" /></p>
 </form>
-<br><br><b>Проверка операции по task_id</b>
+<br><b>Статус операции по task_id</b>
 <form action="action.php" method="get">
- <p>ip ps4: <input type="text" name="ip" value="<?php echo isset($_GET['ip'])?$_GET['ip']:' '; ?>"/></p>
- <p>task_id: <input type="text" name="task_id" value="<?php echo isset($_GET['task_id'])?$_GET['task_id']:' '?>"/></p>
+ <p>ip ps4: <input type="text" name="ip" value="<?php echo isset($_COOKIE['ip'])?$_COOKIE['ip']:' '; ?>"/></p>
+ <p>task_id: <input type="text" name="task_id" value="<?php echo isset($_COOKIE['task_id'])?$_COOKIE['task_id']:' '; ?>"/></p>
  <input type="hidden" name="procedure" value="get_task_progress">
  <p><input type="submit" /></p>
 </form>
-<br><br>
+<br>
  </body>
 </html>
 
 <?php
-
-	$package = isset($_GET['package'])?$_GET['package']:' ';
-	$ip = isset($_GET['ip'])?$_GET['ip']:' ';
-	$procedure = isset($_GET['procedure'])?$_GET['procedure']:' ';
-	$task_id = intval(isset($_GET['task_id'])?$_GET['task_id']:' ');
+	$package = isset($_GET['package'])?trim($_GET['package']):' ';
+	$ip = isset($_GET['ip'])?trim($_GET['ip']):' ';
+	$procedure = isset($_GET['procedure'])?trim($_GET['procedure']):' ';
+	$task_id = intval(isset($_GET['task_id'])?trim($_GET['task_id']):' ');
 
 	if (isset($_GET['procedure']) && isset($_GET['ip'])) {
 
@@ -58,7 +66,7 @@
 	$status = parse("status",$result);
 
 	if ($status == 'fail') {$error = parse("error",$result); $error_r = parse("error_code",$result); echo "Что то пошло не так, ошибка: $error, $error_r";}
-	if ($status == 'success' && $procedure == 'install') {$task_id = parse("task_id",$result); $title = parse("title",$result); echo "Успешно, игра: $title устанавивается, task_id: $task_id";}
+	if ($status == 'success' && $procedure == 'install') {$task_id = parse("task_id",$result); $title = parse("title",$result); echo "Успешно, игра: $title устанавивается, task_id: $task_id"; echo '<br><b><p><a href="'."?ip=$ip&task_id=$task_id&procedure=get_task_progress".'">Проверить операцию task_id: '.$task_id.'</a></p></b>';}
 	if ($status == 'success' && $procedure == 'get_task_progress') {
 		$length = round((hexdec(parse("length",$result)))/1000000000, 3);
 		$transferred = round((hexdec(parse("transferred",$result)))/1000000000, 3);
@@ -77,11 +85,9 @@
 		echo "Устанавливается файл №$num_index из $num_total, осталось всего времени $rest_sec_total мин., $percents%. Скопировалось $transferred_total ГБ из $length_total ГБ";
 	}
 
-} else echo 'Нет связи с приставкой, либо не установлен cURL.';
+} else echo 'Нет связи с PS4, либо не установлен cURL.';
 
-
-} else echo 'Укажите IP и URL для установки или task_id';
-
+} else echo 'Укажите IP и URL для установки или task_id для получения статуса установки';
 
 	function parse($arg,$string){
 	if(preg_match("/\"$arg\": (.*?),/",str_replace('}', ",", $string),$matches)){ $text = str_replace('"', "", $matches[1]);} else {$text = '';}
@@ -90,5 +96,4 @@
 ?>
 <br><br>
 <form method="link" action="javascript:document.location.reload()"><input type="submit" value="update" onClick="this.value = 'updating...'"></form></p>
-
-<br><br><br> Developed by <b>Sc0rpion</b>
+<br><br> Developed by <b>Sc0rpion</b>, v0.02
